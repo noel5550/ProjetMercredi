7620f9cad5a59f249531b357bb6f40973766b201
noel5550 <noel.nyunting@gmail.com>
2018-09-19 13:05:13 +0200

    1er Push

---
+++ index.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Super Projet de Mercredi 1</h1>
+
+
+<a href="pageVid1.php">video 1</a>
+
+<a href="pageVid2.php">video 2</a>
+
+<a href="pageVid3.php">video 3</a>
+
---
+++ pageVid3.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Super Projet de Mercredi 1</h1>
+
+<p>Vidéo 1</p>
+
+<!--  <iframe width="420" height="315" -->
+<!-- src="https://www.youtube.com/embed/xSGW7CwD5GM"> -->
+
+<!-- </iframe>  -->
+
+<video width="320" height="240" controls>
+  <source src="video3.mp4" type="video/mp4">
+  Your browser does not support the video tag.
+</video>
+
+<a href="vid1.mp4" download> 
+<button type="submit" name="download" class="btn"><i class="fa fa-download"></i> Download</button>
+</a>
+
+<a href="index.php">index</a>
+<a href="pageVid2.php">video 2</a>
+<a href="pageVid1.php">video 1</a>
