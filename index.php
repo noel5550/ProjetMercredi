edf074af8f513d8ba73237c00f7d5dbdfb2bc9f7
E152962N <E152962N@I011pc11.ensinfo.sciences.univ-nantes.prive>
2018-09-26 13:29:56 +0200

    azazaza

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <html>
 <head>
 <meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
 <link rel="stylesheet" href="styleaccueil.css" />
 <title>Projet</title>
 
 <body>
 
 	<!--         <h1>projet mercredi</h1> -->
-	<br>
 	<br>
 
 	<!--         <a href="video.php">video</a> -->
@@ -17,9 +19,27 @@
 	<!--         <a href="galerieMulti.php">galerie avec plein d'image de different moment de la video :)</a>  -->
 
 
+<!-- Sidebar/menu -->
+<nav class="w3-sidebar w3-red w3-collapse w3-top w3-large w3-padding" style="z-index:3;width:300px;font-weight:bold;" id="mySidebar"><br>
+  <div class="w3-container">
+    <h3 class="w3-padding-64"><b>Vidéo</b></h3>
+  </div>
+  <div class="w3-bar-block">
+    <a href="index.php" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Accueil</a> 
+  </div>
+</nav>
+<header class="w3-container w3-top w3-hide-large w3-red w3-xlarge w3-padding">
+  <a href="javascript:void(0)" class="w3-button w3-red w3-margin-right" onclick="w3_open()">☰</a>
+  <span>Projet Mercredi</span>
+</header>
 
-	<h1>Gallerie des videos</h1>
+<div class="w3-main" style="margin-left:400px;margin-right:40px">
 
+
+<div class="w3-container" style="margin-top:20px" id="showcase">
+	<h1 class="w3-jumbo"><b>Galerie des videos</b></h1>
+</div>
+		<div class="gallery">
         <?php
         $dir = "miniatures"; // changé le chemin
                              // Ouvre un dossier bien connu, et liste tous les fichiers
@@ -37,8 +57,9 @@
             echo "</table>";
             closedir($dh);
         }
-
+        
         ?>
+        </div>
     </body>
 </head>
 </html>
--- video1.php
+++ video1.php
@@ -21,7 +21,7 @@
 
 <header class="w3-container w3-top w3-hide-large w3-red w3-xlarge w3-padding">
   <a href="javascript:void(0)" class="w3-button w3-red w3-margin-right" onclick="w3_open()">☰</a>
-  <span>Projet Mecredi</span>
+  <span>Projet Mercredi</span>
 </header>
 <!-- Effet de superposition lors de l'ouverture de la barre latérale -->
 <div class="w3-overlay w3-hide-large" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
