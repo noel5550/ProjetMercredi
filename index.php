87d21da3df78e8b816aeffe0c7329e2d437497bd
E152962N <E152962N@I019V7pc01.ensinfo.sciences.univ-nantes.prive>
2018-09-26 10:22:30 +0200

    css

--- index.php
+++ index.php
@@ -1,35 +1,6 @@
 <html>
 
-<style>
-body, h1, h2, h3, h4, h5 {
-	font-family: "Poppins", sans-serif
-}
-
-body {
-	font-size: 16px;
-}
-
-.w3-half img {
-	margin-bottom: -6px;
-	margin-top: 16px;
-	opacity: 0.8;
-	cursor: pointer
-}
-
-.w3-half img:hover {
-	opacity: 1
-}
-</style>
 <body>
-<!-- Sidebar/menu -->
-<nav class="w3-sidebar w3-red w3-collapse w3-top w3-large w3-padding" style="z-index:3;width:300px;font-weight:bold;" id="mySidebar"><br>
-  <div class="w3-container">
-    <h3 class="w3-padding-64"><b>Projet du<br>mercredi</b></h3>
-  </div>
-  <div class="w3-bar-block">
-    <a href="#" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Home</a> 
-  </div>
-</nav>
 
 <!--         <h1>projet mercredi</h1> -->
         <br><br>
