b852d079918fb4215568f7f55f49eadd519f8d58
noel5550 <noel.nyunting@gmail.com>
2018-09-26 11:29:47 +0200

    Video2 css

--- video2.php
+++ video2.php
@@ -1,30 +1,59 @@
 <!DOCTYPE html>
 <html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
+<link href="styleVideo.css" rel="stylesheet" media="all" type="text/css"> 
+</head>
 <body>
+<nav class="w3-sidebar w3-red w3-collapse w3-top w3-large w3-padding" style="z-index:3;width:300px;font-weight:bold;" id="mySidebar"><br>
+  <div class="w3-container">
+    <h3 class="w3-padding-64"><b>Navigation</b></h3>
+  </div>
+  <div class="w3-bar-block">
+    <a href="index.php" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Accueil</a>
+    <h6><a href="video1.php" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Vidéo 1</a></h6>
+    <h6><a href="video3.php" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Vidéo 3</a> </h6>
+  </div>
+</nav>
 
-<h1>Super Projet de Mercredi 1</h1>
+<header class="w3-container w3-top w3-hide-large w3-red w3-xlarge w3-padding">
+  <a href="javascript:void(0)" class="w3-button w3-red w3-margin-right" onclick="w3_open()">☰</a>
+  <span>Projet Mecredi</span>
+</header>
+<!-- Effet de superposition lors de l'ouverture de la barre latérale -->
+<div class="w3-overlay w3-hide-large" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
+<div class="w3-main" style="margin-left:340px;margin-right:40px">
 
-<p>Vidéo 2</p>
-<a href="index.php">index</a>
+
 <br>
+ <div class="w3-container" style="margin-top:80px" id="showcase">
+    <h1 class="w3-jumbo"><b>Vidéo et Images</b></h1>
+    <h1 class="w3-xxxlarge w3-text-red"><b>Vidéo 2</b></h1>
+    <hr style="width:50px;border:5px solid red" class="w3-round">
+  </div>
+
+
 <!--  <iframe width="420" height="315" -->
 <!-- src="https://www.youtube.com/embed/xSGW7CwD5GM"> -->
 
 <!-- </iframe>  -->
 
 
-<video width="400" height="300" controls>
+<video width="800" height="500" controls>
     <source src="video/video2.mp4" type="video/mp4">
     Your browser does not support the video tag.
 </video>
 
-
+<br>
 
 <a href="video/video2.mp4" download>
-    <button type="submit" name="download" class="btn"><i class="fa fa-download"></i> Download</button>
+    <button type="submit" name="download" class="w3-button w3-red w3-padding-large w3-hover-black"><i class="fa fa-download"></i> Téléchargement</button>
 </a>
 
-
+ <h3 class="w3-xxxlarge w3-text-red"><b>Images de la vidéo</b></h3>
 <?php
 
 $dir1 = "frames/video2"; // changer le chemin
@@ -54,7 +83,7 @@ if ($dh1 = opendir($dir1))
 }
 
 
-$dir1 = "miniatures"; // changer le chemin
+/*$dir1 = "miniatures"; // changer le chemin
 // Ouvre un dossier bien connu, et liste tous les fichiers
 if ($dh1 = opendir($dir1))
 {
@@ -74,5 +103,5 @@ if ($dh1 = opendir($dir1))
     echo "</tr>";
     echo "</table>";
     closedir($dh1);
-}
+}*/
 ?>
